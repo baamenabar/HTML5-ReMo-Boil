4deda88490bb1c2e2ea744e70888951955da3e3b
Agustin Amenabar <agustin@fliin.com>
2012-01-11 16:11:55 -0300

    restaurar GA con JS
    Signed-off-by: Agustin Amenabar <agustin@fliin.com>

--- _/inc/javascript-al-pie.php
+++ _/inc/javascript-al-pie.php
@@ -6,40 +6,7 @@
 <script>window.jQuery || document.write("<script src='_/js/jquery-1.6.4.min.js'>\x3C/script>")</script>
 
 <!-- this is where we put our custom functions -->
-<script src="_/js/functions.js"></script>
-
-<!-- Asynchronous google analytics; this is the official snippet.
-	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
-<?php 
-
-$GA_ACCOUNT = "MO-XXXXXX-XX";
-$GA_PIXEL = "_/inc/ga.php";
-
-function googleAnalyticsGetImageUrl() {
-	global $GA_ACCOUNT, $GA_PIXEL;
-	$url = "";
-	$url .= $GA_PIXEL . "?";
-	$url .= "utmac=" . $GA_ACCOUNT;
-	$url .= "&utmn=" . time() . rand(0, 0x7fffffff);
-	
-	$referer = $_SERVER["HTTP_REFERER"];
-	$query = $_SERVER["QUERY_STRING"];
-	$path = $_SERVER["REQUEST_URI"];
-	
-	if (empty($referer)) {
-	$referer = "-";
-	}
-	$url .= "&utmr=" . urlencode($referer);
-	
-	if (!empty($path)) {
-	$url .= "&utmp=" . urlencode($path);
-	}
-	
-	$url .= "&guid=ON";
-	
-	return $url;
-}
-?>	 
+<script src="_/js/functions.js"></script> 
 <script>
 
   var _gaq = _gaq || [];
@@ -53,5 +20,3 @@ function googleAnalyticsGetImageUrl() {
   })();
 
 </script>
-<?php $googleAnalyticsImageUrl = googleAnalyticsGetImageUrl(); ?><img src="<?= $googleAnalyticsImageUrl ?>" class="GAmobileGif" />
--->
