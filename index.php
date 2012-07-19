003257b13c73735c394f7487639197c13b922187
Agustin Amenabar <agustin@fliin.com>
2012-07-19 11:50:02 -0400

    reestructurado y actualizacion de ReMoBoil

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-require_once("_/inc/datos-globales.php");/*require() could be faster // require() es probablemente más rápido. */
-?>
-<!doctype html>
+include "_/inc/datos-globales.php";
+?><!doctype html>
 
-<!-- no doy más soporte a ie6, punto. -->
 <!--[if lt IE 8 ]>    <html class="ie ie7 no-js" lang="es"> <![endif]-->
 <!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="es"> <![endif]-->
 <!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="es"> <![endif]-->
@@ -20,111 +18,67 @@ require_once("_/inc/datos-globales.php");/*require() could be faster // require(
 	Always force latest IE rendering engine (even in intranet) & Chrome Frame 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
 	
-	<title><?php echo $nombreMarca; ?></title>
+	<title><?php echo $nombreMarca; ?> &ndash; <?php echo $sloganMarca; ?></title>
 	
-	<meta name="title" content="<?php echo $nombreMarca.': '.$sloganMarca; ?>">
 	<meta name="description" content="">
 	<!-- Google will often use this as its description of your page/site. Make it good or delete it. -->
+	<meta name="author" content="<?php echo $nombreMarca.' '.$webDelAutor; ?>">
+	<meta name="Copyright" content="Copyright <?php echo $nombreMarca.' '.date('Y'); ?>. All Rights Reserved.">
 	
 	<!--<meta name="google-site-verification" content="">
 	 Speaking of Google, don't forget to set your site up: http://google.com/webmasters 
 	 Mejor dejar esto como un recordatorio de agregar el sitio en webmasters, recomiendo usar el archivo independiente de verificación
 	 Better leave this as a reminder, I'd recommend using the downloadable .html to verify with google. -->
-	
-	<meta name="author" content="<?php echo $nombreMarca.' '.$webDelAutor; ?>">
-	<meta name="Copyright" content="Copyright <?php echo $nombreMarca.' '.date('Y'); ?>. All Rights Reserved.">
-
-	<!-- Dublin Core Metadata : http://dublincore.org/ -->
-	<meta name="DC.title" content="<?php echo $nombreMarca; ?>">
-	<meta name="DC.subject" content="<?php echo $sloganMarca; ?>">
-	<meta name="DC.creator" content="<?php echo $autor; ?>">
-	
-	<!--  Mobile Viewport Fix
-	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
-	device-width : Occupy full width of the screen in its current orientation
-	initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-	maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
-	-->
-	<!-- Usar sólo si considera estilos especiales para tamaños más pequeños / Uncomment to use; use thoughtfully!-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
-	<link rel="shortcut icon" href="favicon.ico">
-	<!-- This is the traditional favicon.
-		 - size: 16x16 or 32x32
-		 - transparency is OK
-		 - see wikipedia for info on browser support: http://mky.be/favicon/ 
-		 Aunque en inglés, este artículo es el definitivo http://bit.ly/4vTgof -->
-		 
-	<link rel="apple-touch-icon" href="_/imgs/apple-touch-icon.png">
-	<!-- The is the icon for iOS's Web Clip.
-		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
-		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
-		 - Transparency is not recommended (iOS will put a black BG behind the icon) -->
+	<?php include "views/viewport-fav-setup.php"; ?>
 	
 	<!-- CSS: pantalla, mobile & impreso están en el mismo archivo / screen, mobile & print are all in the same file
-	This isn't a full reset, it adds default and commonly accepted style definitions
-	Este no es un reset completo como el de Eric Mayer, estandariza definiciones comunmente aceptada, como cuerpos de texto. -->
-	<link rel="stylesheet" href="_/css/style.css">
+	This isn't a full reset nor just a full normalize, it adds default and commonly accepted style definitions plus our prefferences
+	generated with Compass
+	Este no es un reset completo como el de Eric Mayer, ni tampoco un normalize solamente estandariza definiciones comunmente aceptada, como cuerpos de texto, más preferencias nuestras. 
+	generado con Compass -->
+	<link rel="stylesheet" href="_/css/styles.css">
 	
-	<!-- all our JS is at the bottom of the page, except for Modernizr which, in this case, is the complete version, but you should build your own according to the project's requirements. -->
-	<script src="_/js/modernizr.custom.16775.js"></script>
+	<!-- all our JS is at the bottom of the page, except for Modernizr and Respond.js. 
+	Todo el javascript debe ir al pie de la página, excepto por modernizr y Respond.js -->
+	<?php include "views/modernizr.php"; ?>
 
 </head>
 
 <body>
 
-<div class="wrapper"><!-- not needed? up to you: http://camendesign.com/code/developpeurs_sans_frontieres -->
+<!-- STICKY FOOTER ENABLED -->
+<div class="amictus">
+	<div class="wrapper">
 
 	<header>
 		
-		<h1><a href="/index.php">Page Title</a></h1>
-		
+		<h1><a href="./">Page Title</a></h1>
+		<div id="logoTop"><img src="" alt="Logotipo <?php echo $nombreMarca; ?>"></div>
 		<nav>
 		
-			<ol>
-				<li><a href="">Nav Link 1</a></li>
-				<li><a href="">Nav Link 2</a></li>
-				<li><a href="">Nav Link 3</a></li>
-			</ol>
+			<ul>
+				<li><a href="#">Nav Link 1</a></li>
+				<li><a href="#">Nav Link 2</a></li>
+				<li><a href="#">Nav Link 3</a></li>
+			</ul>
 		
 		</nav>
 	
 	</header>
 	
-	<article>
+	<div class="corporis">
 		
-		<h1>Article Header</h1>
-		
-		<p>Etiam pretium odio eu mi convallis vitae varius neque pharetra. Nulla vestibulum nisi ut sem cursus sed mattis nisi egestas.</p>
-		
-		<h2>Article Subhead</h2>
-		
-		<p>Vestibulum lacus erat, volutpat vel dignissim at, fringilla ut felis.</p>
-		
-		<h3>Un Sub Tema en H3</h3>
-		<ul>
-		<li><h4>Título en h4</h4><p>para un párrafo en una lista</p></li>
-		<li><h4>Título en h4</h4><p>para un párrafo en una lista</p></li>
-		<li><h4>Título en h4</h4><p>para un párrafo en una lista</p></li>
-		</ul>
-		<h5>H5 para una listilla de sub-tema</h5>
-		<h6>No puedo imaginarme 6 niveles de subtítulos</h6>
-	
-	</article>
-	
-	<aside>
-	
-		<h2>Sidebar Content</h2>
-	
-	</aside>
-	
-	<footer>
-		
+	</div>
+
+	<div class="urgeo"></div>
+</div>
+<footer>	
+	<div class="wrapper">
 		<p><small>&copy; Copyright <?php echo $nombreMarca.' '.date('Y'); ?>. All Rights Reserved.</small></p>
-		
-	</footer>
+	</div>
+</footer>
 
 </div>
-<?php require("_/inc/javascript-al-pie.php"); ?>  
+<?php require("views/javascript-al-pie.php"); ?>  
 </body>
 </html>
--- views/javascript-al-pie.php
+++ views/javascript-al-pie.php
@@ -1,22 +1,18 @@
 
-<!-- here comes the javascript -->
+<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
 
-<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script>window.jQuery || document.write("<script src='_/js/jquery-1.6.4.min.js'>\x3C/script>")</script>
+    <!-- scripts concatenated and minified via build script -->
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+    <!-- end scripts -->
 
-<!-- this is where we put our custom functions -->
-<script src="_/js/functions.js"></script> 
-<script>
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+    <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+         mathiasbynens.be/notes/async-analytics-snippet -->
+    <script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
