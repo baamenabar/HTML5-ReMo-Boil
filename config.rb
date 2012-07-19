217e518157cd7c172c97a2516d5c5b0391a475bb
Agustin Amenabar <agustin@fliin.com>
2012-07-19 07:53:39 -0400

    agregada violeta a humans.txt

--- config.rb
+++ config.rb
@@ -2,10 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "./css"
-sass_dir = "./sass"
-images_dir = "./img"
-javascripts_dir = "./js"
+css_dir = "./_/css"
+sass_dir = "./_/sass"
+images_dir = "./_/imgs"
+javascripts_dir = "./_/js"
+output_style = :expanded
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
