26d73a197e238285a68d05c9693671856ee413b3
Agustin Amenabar <agustin@fliin.com>
2012-08-13 16:42:38 -0400

    added basic typical initial calls to jquery document.ready and window.load
    Signed-off-by: Agustin Amenabar <agustin@fliin.com>

--- _/js/main.js
+++ _/js/main.js
@@ -1,3 +1,17 @@
-/* Author:
+/* Author: Agustin Amenabar
 
 */
+/*Primero declaramos variables generales
+  First we declare general variables//*/
+
+/*Segundo ejecutamos acciones a penas el DOM esté listo
+  Second we ejecute acions on DOM ready */
+$(document).ready(function(){
+	
+});
+  
+/* Tercero: ejecutamos acciones no-escenciales y llamados a contenido externo adicional (como publicidad)
+   Third: we execute the non-escential actions and extra content calls (like adds)*/
+$(window).load(function(){
+		
+});
